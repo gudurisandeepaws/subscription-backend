45fc1d633f3e6a34edadf4a5dda6e33809f45417
gudurisandeepaws <gudurisandeepaws@gmail.com>
2026-05-11 22:39:20 +0530

    state file bucket added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "state-bucket-interview-assistant-application"
+    key            = "backend/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
